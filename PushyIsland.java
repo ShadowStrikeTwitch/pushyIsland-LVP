e8a834aefb4d05459b8a9c1ffe56fc0d4e838893
Leon <leon.sahl@mni.thm.de>
2024-11-25 12:10:11 +0100

    Erste Methoden Ideen

---
+++ PushyIsland.java
@@ -0,0 +1,47 @@
+Clerk.clear();
+
+
+class pushyisland{
+
+    pushyisland(){
+
+    }
+
+    void startGame(){
+
+    }
+
+    void showTitleScreen(){
+
+    }
+
+    String generateWorld(){
+
+        return "Welt wurde generiert!";
+    }
+
+    String showLevelOne(){
+
+        return "[PushyIsland] Level 1";
+    }
+    
+    String showLevelTwo(){
+        
+        return "[PushyIsland] Level 2";
+    }
+
+    String showWinScreen(){
+
+        return "[PushyIsland] Level geschafft.";
+    }
+
+    boolean canWin(){
+        
+    }
+
+    //public String toString(){
+    //
+    //    return "";
+    //}
+
+}
