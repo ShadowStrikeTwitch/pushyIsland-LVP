06e4ef96b226d0e0e1ae6c9a9c16dac332c879b5
Leon <leon.sahl@mni.thm.de>
2024-12-02 13:11:07 +0100

    Move enum und Methode hinzugefügt.

--- PushyIsland.java
+++ PushyIsland.java
@@ -3,152 +3,182 @@ Clerk.clear();
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
+
 
 public class PushyIsland{
     boolean gameRunning;
     int currentLevel;
     Turtle t = new Turtle(1100,700);
-
+    
+    
     public PushyIsland(){
         gameRunning = false;
         currentLevel = 0;
         run();
     }
-
-    void run(){
+    
+    void run(){ // Runtime
         if (gameRunning) {
             drawLevel(generatelevel(currentLevel));
         } else showTitleScreen();
     }
-
-    void startGame(){
+    
+    void startGame(){ // Spiel starten
         gameRunning = true;
         currentLevel = 1;
         run();
     }
-
+    
     void endGame(){ // Spiel 'beenden'
-        System.out.println("[PushyIsland] Spiel beendet." + System.lineSeparator());
-        Clerk.clear();
+    System.out.println("[PushyIsland] Spiel beendet." + System.lineSeparator());
+    Clerk.clear();
     }
 
-    void showTitleScreen(){ // Titelbildschirm
-        t.moveTo(550,190).color(0,100,100);
-        for (int i = 0; i < 360; i++) {
-            t.forward(2).right(1);
-        }
-        t.moveTo(600,300);
-        for (int i = 0; i < 360; i++) {
-            t.forward(0.5).right(1);
-        }
-        t.moveTo(510,290);
-        for (int i = 0; i < 360; i++) {
-            t.forward(0.75).right(1);
-        }
-        t.moveTo(525,340);
-        for (int i = 0; i < 360; i++) {
-            t.forward(0.1).right(1);
-        }
-        t.moveTo(600,330);
-        for (int i = 0; i < 360; i++) {
-            t.forward(0.1).right(1);
-        }
-
-        t.left(90).color(0,200,200);
-        t.textSize = 85;
-        t.moveTo(550, 100).text("PushyIsland - remake");
-        t.textSize = 20;
-        t.moveTo(750, 130).color(0,255,200).text("by Leon Sahl");
-        t.textSize = 50;
-        t.color(150,100,255).moveTo(550, 500);
-        t.text("Spiel Starten mit p.startGame").moveTo(550, 600).text("Spiel Verlassen mit p.endGame");
+void showTitleScreen(){ // Titelbildschirm
+    t.moveTo(550,190).color(0,100,100);
+    for (int i = 0; i < 360; i++) {
+        t.forward(2).right(1);
     }
-
-    level generatelevel(int currentLevel){ // Einzelne Level generieren
-        level l = new level(currentLevel);
-        System.out.println("[PushyIsland] Level " + l.level + " wird geladen." + System.lineSeparator());
-        switch (currentLevel) {
-            case 1:
-                l.loadWorld(l.level1); // Level 1 laden
-                break;
-            case 2:
-                l.loadWorld(l.level2); // Level 2 laden
-                break;
-            case 3:
-                l.loadWorld(l.level3); // Level 3 laden 
-                break;
-            case 4:
-                l.loadWorld(l.level4); // Level 4 laden
-                break;
-            case 5:
-                l.loadWorld(l.level5); // Level 5 laden
-                break;
-            case 6:
-                l.loadWorld(l.level5); // Level 6 laden
-                break;
-            default: // Wenn kein Level mehr gefunden wird, wird das Spiel beendet
-                currentLevel = 0;
-                clearScreen();
-                break;
-        }
-        try {
-            Thread.sleep(500); //"Lade" Zeit
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return l;
+    t.moveTo(600,300);
+    for (int i = 0; i < 360; i++) {
+        t.forward(0.5).right(1);
     }
-
-    void clearScreen(){ // Bildschirm löschen
-        Clerk.clear();
-        Clerk.markdown("PushyIsland - remake");
+    t.moveTo(510,290);
+    for (int i = 0; i < 360; i++) {
+        t.forward(0.75).right(1);
+    }
+    t.moveTo(525,340);
+    for (int i = 0; i < 360; i++) {
+        t.forward(0.1).right(1);
+    }
+    t.moveTo(600,330);
+    for (int i = 0; i < 360; i++) {
+        t.forward(0.1).right(1);
     }
     
-    void drawLevel(level l){ //Level anzeigen
-        StringBuilder s = new StringBuilder();
-        int tx = 50, ty = 50;
-        t.reset();
-        t.left(90).textSize = 50;
-        for (int i = 0; i < l.world.length; i++) {
-            for (int j = 0; j < l.world[i].length; j++) {
-                switch (l.world[i][j]) {
-                    case 0:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 42).text("🟦");  // oder: ♒︎
-                        break;
-                        case 1:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 42).text("🟨");
-                        break;
-                        case 2:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 44).text("🗻");
-                        break;
-                        case 3:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 42).text("🌴");
-                        break;
-                        case 4:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 42).text("📦");
-                        break;
-                        case 5:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 42).text("🟩");
-                        break;
-                        case 7:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 42).text("😋");
-                        break;
-                        case 8:
-                        t.color(0,0,205).moveTo(tx * j + 25, ty * i + 43).text("🏠");
-                        break;
-                        default:
-                        t.moveTo(tx * j + 25, ty * i + 42).text("❌");
-                        break;
+    t.left(90).color(0,200,200);
+    t.textSize = 85;
+    t.moveTo(550, 100).text("PushyIsland - remake");
+    t.textSize = 20;
+    t.moveTo(750, 130).color(0,255,200).text("by Leon Sahl");
+    t.textSize = 50;
+    t.color(150,100,255).moveTo(550, 500);
+    t.text("Spiel Starten mit p.startGame").moveTo(550, 600).text("Spiel Verlassen mit p.endGame");
+}
+
+
+level generatelevel(int currentLevel){ // Einzelne Level generieren
+    level l = new level(currentLevel);
+    System.out.println("[PushyIsland] Level " + l.level + " wird geladen." + System.lineSeparator());
+    switch (currentLevel) {
+        case 1:
+        l.loadWorld(l.level1); // Level 1 laden
+        break;
+        case 2:
+        l.loadWorld(l.level2); // Level 2 laden
+        break;
+        case 3:
+        l.loadWorld(l.level3); // Level 3 laden 
+        break;
+        case 4:
+        l.loadWorld(l.level4); // Level 4 laden
+        break;
+        case 5:
+        l.loadWorld(l.level5); // Level 5 laden
+        break;
+        case 6:
+        l.loadWorld(l.level5); // Level 6 laden
+        break;
+        default: // Wenn kein Level mehr gefunden wird, wird das Spiel beendet
+        currentLevel = 0;
+        clearScreen();
+        break;
+    }
+    try {
+        Thread.sleep(500); //"Lade" Zeit
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    return l;
+}
+
+void clearScreen(){ // Bildschirm löschen
+    Clerk.clear();
+    Clerk.markdown("PushyIsland - remake");
+}
+
+void drawLevel(level l) { // Level anzeigen
+    int tx = 50, ty = 50;
+    t.reset().left(90).textSize = 50;
+    for (int i = 0; i < l.world.length; i++) {
+        for (int j = 0; j < l.world[i].length; j++) {
+            int x = tx * j + 25, y = ty * i + 42;
+            String emoji = switch (l.world[i][j]) {
+                case 0 -> "🟦"; // Wasser
+                case 1 -> "🟨"; // Boden
+                case 2 -> "🟩"; // Erhöhung
+                case 3 -> switch (new Random().nextInt(4)) {
+                    case 0 -> "🌳"; // Baum
+                    case 1 -> {
+                        y += 2; yield "🗻"; // Berg
                     }
-                }
-            }
+                    case 2 -> "🌴"; // Palme
+                    default -> "🍄"; // Pilz
+                };
+                case 4 -> "📦"; // Box
+                case 7 -> "😋"; // Spieler
+                case 8 -> { y += 1; yield "🏠"; } // Haus
+                default -> "❌"; // Fehler
+            };
+            t.color(0, 0, 205).moveTo(x, y).text(emoji);
+        }
+    }
+}
+
+
+        
+        void levelPassed(){
+            System.out.println("[PushyIsland] Level geschafft." + System.lineSeparator());
+            currentLevel++;
+            run();
         }
 
-    void levelPassed(){
-        System.out.println("[PushyIsland] Level geschafft." + System.lineSeparator());
-        currentLevel++;
-        run();
+        boolean rules(){
+
+
+
+            return false;
+        }
+        
+        Move move(Move m){
+            if (gameRunning) {
+                switch (m) {
+                    case UP:
+                    
+                    break;
+                    case DOWN:
+                    
+                    break;
+                    case LEFT:
+                    
+                    break;
+                    case RIGHT:
+                    
+                    break;
+                default:
+                break;
+            }
+        }
+        return m;
     }
+}
+
+public enum Move{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT;
 }
 
 class level{
--- reset.java
+++
@@ -1,8 +0,0 @@
-/reset
-
-/o lvp.java
-
-Clerk.view();
-
-/o PushyIsland.java
-/o PushyIsland.java
